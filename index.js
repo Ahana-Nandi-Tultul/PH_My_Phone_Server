25616c11fd54e84a272535d403c68c68ccfaeb61
Ahana-Nandi-Tultul <83442291+Ahana-Nandi-Tultul@users.noreply.github.com>
2023-08-03 21:33:54 +0600

    First API

---
+++ index.js
@@ -0,0 +1,28 @@
+const express = require('express');
+const app = express();
+const cors = require('cors');
+const phones = require('./phones.json');
+const port = 5000;
+
+app.use(cors());
+
+app.get('/', (req, res) => {
+    res.send(`Phone Data is comming`);
+});
+
+app.get('/phones', (req, res) => {
+    res.send(phones)
+})
+
+app.get('/phones/:id', (req, res) => {
+    const id = parseInt(req.params.id);
+    console.log(`I need id: ${id}`);
+
+    const phone = phones.find(phone => phone.id === id)
+    res.send(phone);  
+})
+
+app.listen(port, () => {
+    console.log(`My Server is: ${port}`);
+});
+
